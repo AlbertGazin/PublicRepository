abf1ef5ae23190564d2bbaeb3ad1a9920816ee0a
Albert Gazin <gazin.albert@mail.ru>
2022-10-04 21:41:40 +0300

    spring boot training

---
+++ spring-boot-app/src/main/java/com/albertgazin/spring/springbootapp/models/Post.java
@@ -0,0 +1,71 @@
+package com.albertgazin.spring.springbootapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String title;
+
+    private String announce;
+
+    private String fullText;
+
+    private int viewsCount;
+
+    public Post(String title, String announce, String fullText) {
+        this.title = title;
+        this.announce = announce;
+        this.fullText = fullText;
+    }
+
+    public Post() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnnounce() {
+        return announce;
+    }
+
+    public void setAnnounce(String announce) {
+        this.announce = announce;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public int getViewsCount() {
+        return viewsCount;
+    }
+
+    public void setViewsCount(int viewsCount) {
+        this.viewsCount = viewsCount;
+    }
+}
